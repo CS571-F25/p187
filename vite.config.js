ac6af297ab0ce311b277ad9318eb82b43eb29936
gryske-soph <gryske@wisc.edu>
2025-10-27 21:33:06 -0500

    troubleshooting inital load

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-plugins: [react()],
-base: '/p187/',
-build: {
-outDir: 'docs'
-}
+  plugins: [react()],
+  base: '/p187/',
+  build: {
+    outDir: 'docs'
+  }
 })
